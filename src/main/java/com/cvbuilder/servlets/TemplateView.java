369b2cde92698edf4a2e209c22165ff70cd7b041
timothelaine <contact@timothe.laine.com>
2020-10-06 16:50:56 +0200

    up

--- src/main/java/com/cvbuilder/servlets/TemplateView.java
+++ src/main/java/com/cvbuilder/servlets/TemplateView.java
@@ -1,4 +1,30 @@
 package com.cvbuilder.servlets;
 
-public class templateView {
+import com.cvbuilder.entity.DB;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "template", urlPatterns = {"/Template"})
+public class TemplateView extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Ko");
+        if(request.getParameter("id") != null) {
+            System.out.println("OK");
+            com.cvbuilder.entity.Template template = null;
+            EntityManager entityManager = DB.getEntityManager();
+            template = entityManager.find(com.cvbuilder.entity.Template.class, Long.parseLong(request.getParameter("id")));
+            request.setAttribute("template", template);
+            this.getServletContext().getRequestDispatcher("/view/template.jsp").forward(request, response);
+        }
+    }
 }
--- src/main/webapp/view/template.jsp
+++ src/main/webapp/view/template.jsp
@@ -8,9 +8,9 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>$Title$</title>
+    <title>template</title>
 </head>
 <body>
-$END$
+<c:out value="${template.getCode()}"/>
 </body>
 </html>
